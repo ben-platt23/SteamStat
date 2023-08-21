5dfd0470d6dada791b566209111e0e81b89472e4
Ben Platt <86609189+ben-platt23@users.noreply.github.com>
2023-08-21 16:44:50 -0400

    Remove private keys and info from public repo

--- WebDev2_SemProject/PRIVATE/DBInfo.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    // Cloud IP
-    $IP = "54.162.136.97";
-    // Local dev IP
-    // $IP = "192.168.1.160";
-    $username = "btplatt";
-    $password = "password123";
-    $SchemaName = "steamstat";
-?>
